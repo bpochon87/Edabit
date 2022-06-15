86f5f6cfb3253c6c952c4946080f49ad1810b4d6
bpochon87 <83652607+bpochon87@users.noreply.github.com>
2022-06-15 12:29:31 -0600

    June 15, 2022 practice

--- Easy/Program.cs
+++ Easy/Program.cs
@@ -48,11 +48,71 @@ public class Program
         }
     }
 
+    // June 15, 2022
+    // Easy
+
+    // Create a function that takes an array of number and returns a new array sorted in ascending order.
+    public void SortNumAscending()
+    {
+        int[] arr = { 15, 3, 42, 7, 59, 0, 21 };
+        Array.Sort(arr);
+        // string.Join(Char, Object[]) concatenates the string representations of an array of objects
+        // using the specified separator between each member.
+        Console.WriteLine($"{string.Join(", ", arr )}");;
+    }
+
+    // Given two integers, return [true] if [a] can be divided evenly by [b]. Return [false] otherwise.
+    public void DividesEvenly()
+    {
+        Console.WriteLine("Please enter a number: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Please enter another number: ");
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        if (x % y == 0)
+        {
+            Console.WriteLine($"{x} can be evenly divided by {y}. This is true");
+        }
+        else
+        {
+            Console.WriteLine($"{x} cannot be evenly divided by {y}. This is false.");
+        }
+    }
+
+    // Return negative number. If negative, keep negative.
+    public void ReturnNegative()
+    {
+        System.Console.WriteLine("Please enter a number: ");
+        int userNum = Convert.ToInt32(Console.ReadLine());
+        int negativeNum = userNum < 0 ? userNum : -userNum;
+        System.Console.WriteLine($"Your number negative is { negativeNum }.");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public static void Main(string[] args)
     {
         Program pr = new Program();
         pr.Factorial();
         pr.FizzBuzz();
+        pr.SortNumAscending();
+        pr.DividesEvenly();
+        pr.ReturnNegative();
     }
 
 
