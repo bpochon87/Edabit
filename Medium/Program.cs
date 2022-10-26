4ea5b153056cb0dbeac49c3a012de1d295cdab14
Brady Pochon <bpochon@gmail.com>
2022-10-26 08:34:10 -0600

    Add daily practice

--- Medium/Program.cs
+++ Medium/Program.cs
@@ -75,32 +75,69 @@ public class Program
     //     Console.ReadLine();
     // }
 
-    // October 24, 2022
+    // // October 24, 2022
+    // // Medium
+
+    // // Create a function that tests whether or not an integer is a perfect number. 
+    // // A perfect number is a number that can be written as the sum of its factors,
+    // // (equal to sum of its proper divisors) excluding the number itself.
+
+    // public void FactorsAddUp()
+    // {
+    //     int num = 6;
+    //     int[] factors = {1, 2, 3};
+    //     int sum = 0;
+
+    //     for (int i = 1; i < num; i++)
+    //     {
+    //         if (num % i == 0)
+    //         {
+    //             sum += i;
+    //         }
+    //         else
+    //         {
+    //             continue;
+    //         }
+    //     }
+    //     System.Console.WriteLine(sum == num);
+    // }
+
+    // October 26, 2022
     // Medium
 
-    // Create a function that tests whether or not an integer is a perfect number. 
-    // A perfect number is a number that can be written as the sum of its factors,
-    // (equal to sum of its proper divisors) excluding the number itself.
+    // Given an array of integers, return the largest gap between elements of the sorted version of that array.
 
-    public void FactorsAddUp()
+    // public void LargestGap()
+    // {
+    //     int[] arr = {9, 4, 26, 26, 0, 0, 5, 20, 6, 25, 5};
+
+    //     Array.Sort(arr);
+    //     int difference = 0;
+        
+    //     for (int i = 0, j = 1; i < arr.Length - 1; i++, j++)
+    //     {
+    //         int currGap = Math.Abs(arr[i] - arr[j]);
+    //         if (currGap > difference)
+    //         {
+    //             difference = currGap;
+    //         }
+    //         else
+    //         {
+    //             continue;
+    //         }
+    //     }
+    //     System.Console.WriteLine(difference);
+    // }
+
+    // October 26, 2022
+    // Medium
+
+    // Without using conditionals, ternary operators, etc., flip one to zero and zero to one.
+
+    public int Flip(int y)
     {
-        int num = 6;
-        int[] factors = {1, 2, 3};
-        int sum = 0;
-
-        for (int i = 1; i < num; i++)
-        {
-            if (num % i == 0)
-            {
-                sum += i;
-            }
-            else
-            {
-                continue;
-            }
-        }
-        System.Console.WriteLine(sum == num);
-    } 
+        return Convert.ToInt32(!(Convert.ToBoolean(y)));
+    }
 
     
     public static void Main()
@@ -109,7 +146,10 @@ public class Program
         // pr.ArrayOfMultiples();
         // pr.IndexOfCapitals();
         // pr.FindNemo();
-        pr.FactorsAddUp();
+        // pr.FactorsAddUp();
+        // pr.LargestGap();
+        Console.WriteLine(pr.Flip(0));
+        Console.ReadLine();
     }
 
 
